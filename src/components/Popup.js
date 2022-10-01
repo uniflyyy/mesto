7301bdad85d6ccead8c721587d8b610ad754da10
Dmitry Kononenko <diman-kononenko@yandex.ru>
2022-10-01 16:17:50 +0300

    Project 8. Some fixes after first review.

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -17,12 +17,12 @@ export default class Popup {
 
     open() {
         this._popup.classList.add('popup_opened');
-        document.addEventListener('keydown', this._handleEscClose);
+        document.addEventListener('keydown', this._handleEscClose.bind(this));
     }
 
     close() {
         this._popup.classList.remove('popup_opened');
-        document.removeEventListener('keydown', this._handleEscClose);
+        document.removeEventListener('keydown', this._handleEscClose.bind(this));
     };
 
     setEventListeners() {
