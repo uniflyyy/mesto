9f4a6c4a5092fd1b61f4c459fb25802c33cf630e
Dmitry Kononenko <diman-kononenko@yandex.ru>
2022-10-01 19:39:34 +0300

    Project 8. Fix popup js after 2nd review.

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,7 @@
 export default class Popup {
     constructor(popupSelector) {
         this._popup = document.querySelector(popupSelector);
+        this._handleEscClose = this._handleEscClose.bind(this);
     }
 
     _handleEscClose(evt) {
@@ -17,12 +18,12 @@ export default class Popup {
 
     open() {
         this._popup.classList.add('popup_opened');
-        document.addEventListener('keydown', this._handleEscClose.bind(this));
+        document.addEventListener('keydown', this._handleEscClose);
     }
 
     close() {
         this._popup.classList.remove('popup_opened');
-        document.removeEventListener('keydown', this._handleEscClose.bind(this));
+        document.removeEventListener('keydown', this._handleEscClose);
     };
 
     setEventListeners() {
